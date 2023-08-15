f57ce80c0ab9b418949f7c0a35c28a7603904e09
mrmetapod2 <hectormzacrias@gmail.com>
2023-08-15 19:15:03 -0300

    avance

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //References
+    public CircleCollider2D collider1; //hitbox de fisicas del jugador en si
+    public CircleCollider2D collider2; //hitbox para pegarse a la plataforma el mas grande
+    private Collider2D plataforma; //plataforma que se encuentra en el rango de poder pegarse al jugador
+    private Collider2D plataformaPegada; //plataforma en la que esta pegado el jugador
+    bool pegado_status; //si esta pegado el jugador o no
+    private Vector3 relative_position; //posicion relativa entre el jugador y la plataforma a la que esta pegado
+    private Vector3 PrevPlatPos; //posicion del frame anterior de la plataforma
+    private Vector3 Desplazo; //espacio transportado por la plataforma entre un frame y el siguiente
+    
     [SerializeField] private Rigidbody2D rb_player;
 
     [SerializeField] private float moveSpeed;
@@ -30,6 +39,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
        xInput = Input.GetAxisRaw("Horizontal");
        yInput = Input.GetAxisRaw("Vertical");
 
@@ -50,6 +60,33 @@ public class PlayerController : MonoBehaviour
        {
             Jump();
        }
+
+       //codigo para pegarse con E
+
+       if(Input.GetKeyDown(KeyCode.E) && !pegado_status && plataforma!=null){
+        plataformaPegada=plataforma;
+        pegado_status=true;
+       
+        relative_position= transform.position - plataformaPegada.transform.position;
+        transform.position= plataformaPegada.transform.position+ relative_position;
+        rb_player.gravityScale =0f;
+
+       }
+       else if(Input.GetKeyDown(KeyCode.E) && pegado_status && plataformaPegada!=null){
+         plataformaPegada=null;
+         pegado_status=false;
+         rb_player.gravityScale =1;
+
+         rb_player.AddForce(Desplazo);
+
+       }
+       if(pegado_status== true && plataformaPegada!= null){
+       
+        transform.position= plataformaPegada.transform.position+ relative_position;
+        FindPlatSpeed();
+        
+       }
+       
     }
 
     private void FixedUpdate()
@@ -62,4 +99,30 @@ public class PlayerController : MonoBehaviour
         rb_player.AddForce(Vector2.up * jumpForce);
     }
 
+    //chequear si hay alguna plataforma pegable alrededor del jugador
+
+     private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if (other.tag=="MovPlat"){
+            plataforma=other;
+           
+        }
+        else
+        {
+            plataforma=null;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        plataforma=null;
+    }
+    //encontrar la velocidad de la plataforma en la que estas conectado
+    private void FindPlatSpeed(){
+       Desplazo =plataformaPegada.transform.position-PrevPlatPos;
+       PrevPlatPos=plataformaPegada.transform.position;
+       Desplazo= Desplazo*Time.deltaTime;
+      
+    }
 }
